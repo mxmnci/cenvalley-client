1d49ed3b54b64d178409d032b0163150319af5a0
Max Monciardini <38739342+maxmonciardini@users.noreply.github.com>
2021-07-10 18:28:49 -0700

    Fixed contact links

--- src/components/ContactInfoButton.js
+++ src/components/ContactInfoButton.js
@@ -1,19 +1,17 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Link } from "gatsby";
 
 const ContactInfoButton = ({ type, info, link, icon, small = false }) => {
   return (
     <div className={`inline-flex items-center ${small ? "mb-4" : "mb-6"}`}>
-      <Link to={link}>
-        <motion.div
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.9 }}
-          className={`bg-yellow ${small ? "p-2" : "p-3"} rounded mr-4`}
-        >
-          {icon}
-        </motion.div>
-      </Link>
+      <motion.a
+        href={link}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className={`bg-yellow ${small ? "p-2" : "p-3"} rounded mr-4`}
+      >
+        {icon}
+      </motion.a>
       <div className="flex flex-col text-white">
         <h5 className={`font-bold ${small ? "text-md" : "text-xl"}`}>{type}</h5>
         <span className={small ? "text-sm" : "text-lg"}>{info}</span>
