90a182dbb8b2c36e1cbbab99974c50ce5f1b478e
Max Monciardini <38739342+mxmnci@users.noreply.github.com>
2022-02-03 16:40:14 -0800

    fix: og:image incorrect configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     description:
       "Established in 2000, Central Valley Engineering & Asphalt is a regional construction leader. We exist to build smooth, long-lasting surfaces that improve the curb appeal and value of your property while adding the durability and practicality your surfaces need.",
     url: "https://cenvalley.com",
-    image: "/static/images/cenvalley.jpg"
+    image: "/cenvalley.jpg",
   },
   plugins: [
     "gatsby-plugin-postcss",
@@ -20,8 +20,8 @@ module.exports = {
         background_color: "#252525",
         theme_color: "#FFFF63",
         display: "standalone",
-        icon: "src/images/logo.svg"
-      }
+        icon: "src/images/logo.svg",
+      },
     },
     {
       resolve: `gatsby-plugin-sharp`,
@@ -35,8 +35,8 @@ module.exports = {
         forceBase64Format: "png", // valid formats: png,jpg,webp
         useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
         stripMetadata: true,
-        defaultQuality: 50
-      }
-    }
-  ]
+        defaultQuality: 50,
+      },
+    },
+  ],
 };
